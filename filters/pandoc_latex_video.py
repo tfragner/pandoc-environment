35984466bce59699875d329b7bd32131e7f76627
tfragner <fragner@bitter.at>
2018-10-08 19:47:23 +0200

    Create pandoc_latex_video.py

---
+++ filters/pandoc_latex_video.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import panflute as pf
+
+"""
+Pandoc filter that renders QR Codes for youtube videos in LaTex
+and an iframe in HTML
+"""
+
+
+def latex(s):
+    return pf.RawInline(s, format='latex')
+
+def html(s):
+    return pf.RawInline(s, format='html')
+
+def youtubevideo(e, doc):
+    if type(e) == pf.Link and doc.format == 'latex':
+        if 'youtube.com' in e.url or 'youtu.be' in e.url:
+            if "for-image" in e.attributes:
+                youtube = latex('\\vspace{-2.5em}\\todo[caption=' + pf.stringify(e) + ', color=black!0, linecolor=blue!50]{\\qrcode[height=1.75cm]{' + e.url + '}}')
+            else:
+                youtube = latex('\\todo[caption=' + pf.stringify(e) + ', color=black!0, linecolor=blue!50]{\\qrcode[height=1.75cm]{' + e.url + '}}')
+            return [youtube]
+    elif type(e) == pf.Link and doc.format == 'html':
+        if 'youtube.com' in e.url or 'youtu.be' in e.url:
+            youtube = html('<iframe height="250" src="' + e.url + '"></iframe>')
+            return [youtube]
+
+if __name__ == "__main__":
+    pf.toJSONFilter(youtubevideo)
