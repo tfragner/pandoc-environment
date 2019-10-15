75d6e67185d15a6c5b64cbd4f32a239612a6202e
Thomas Fragner <fragner@bitter.at>
2019-10-15 11:07:29 +0200

    added filter to add version table

---
+++ filters/pandoc-add-version-table.py
@@ -0,0 +1,56 @@
+"""
+Set all headers to level 1
+"""
+
+from panflute import *
+from collections import OrderedDict
+
+last = []
+
+def action(elem, doc):
+    if isinstance(elem, Div):
+        meta = doc.get_metadata()
+        debug(meta)
+        last.append(meta['versionen'][-1])
+        rows = []
+        versions = []
+        for x in meta['versionen']:
+            version = Para(Str(x['version']))
+            ersteller = Para(Str(x['ersteller']))
+            erstelldatum = Para(Str(x['erstelldatum']))
+            pruefer = Para(Str(x['pruefer']))
+            pruefdatum = Para(Str(x['pruefdatum']))
+            freigeber = Para(Str(x['freigeber']))
+            freigabedatum = Para(Str(x['freigabedatum']))
+            versions.append(TableRow(TableCell(Para(Str(x['version']))), TableCell(Para(Str(x['kurzbeschreibung'])))))
+            rows.append(TableRow(TableCell(version), TableCell(ersteller), TableCell(erstelldatum), TableCell(pruefer), TableCell(pruefdatum), TableCell(freigeber), TableCell(freigabedatum)))
+
+        header = TableRow(TableCell(Para(Str('Ver'))), TableCell(Para(Str('Ersteller'))), TableCell(Para(Str('Datum'))), TableCell(Para(Str('Prüfer'))), TableCell(Para(Str('Datum'))), TableCell(Para(Str('Freigeber'))), TableCell(Para(Str('Datum'))))
+        table = Table(*rows, header=header,width=[1,2,4,2,4,2,4])
+        header2 = TableRow(TableCell(Para(Str('Ver'))), TableCell(Para(Str('Ersteller'))))
+        table2 = Table(*versions, header=header2, width=[1,10])
+        
+        h1 = Header(Str("Versionen"), level=3)
+        h2 = Header(Str("Änderungen"), level=3)
+        return [h1, table, h2, table2]
+
+        
+def finalize(doc):
+    author = MetaString(last[0]['ersteller'])
+    date = MetaString(last[0]['erstelldatum'])
+    kurzbeschreibung = MetaString(last[0]['kurzbeschreibung'])
+    doc.metadata['author'] = author
+    doc.metadata['ersteller'] = MetaString(last[0]['ersteller'])
+    doc.metadata['erstelldatum'] = MetaString(last[0]['erstelldatum'])
+    doc.metadata['pruefer'] = MetaString(last[0]['pruefer'])
+    doc.metadata['pruefdatum'] = MetaString(last[0]['pruefdatum'])
+    doc.metadata['freigeber'] = MetaString(last[0]['freigeber'])
+    doc.metadata['freigabedatum'] = MetaString(last[0]['freigabedatum'])
+    doc.metadata['date'] = date
+    doc.metadata['kurzbeschreibung'] = kurzbeschreibung
+
+def main(doc=None):
+    return run_filters(actions=[action], doc=doc, finalize=finalize)
+
+if __name__ == '__main__':
+    main()
